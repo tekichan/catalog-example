4ef2d742e0c04dbf56816d70e9790d2962cbd09d
Teki Chan <teki.chan@lxpo02t23ddgtdx.brandloyaltyint.corp>
2020-10-15 11:16:17 +0800

    Updates

---
+++ precache-manifest.4700610f985767451b0dc8a7dba7874f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c73adc6a876213b50f26272fe38c8395",
+    "url": "/catalog-example/index.html"
+  },
+  {
+    "revision": "ce41e486021a4e548872",
+    "url": "/catalog-example/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7aace097712adc7ae282",
+    "url": "/catalog-example/static/js/2.f09f5aa1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/catalog-example/static/js/2.f09f5aa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce41e486021a4e548872",
+    "url": "/catalog-example/static/js/main.f6343b85.chunk.js"
+  },
+  {
+    "revision": "f159ab8f6de3977a74d6",
+    "url": "/catalog-example/static/js/runtime-main.d4f44a13.js"
+  }
+]);
